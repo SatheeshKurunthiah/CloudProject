8027add6f790f47adf86f4fdd3cb98505d4c60dd
Satheesh Kurunthiah <satheeshkurunthiah@Satheeshs-MacBook-Pro.local>
2018-03-13 11:28:42 -0700

    Added simple php to process request

---
+++ API/ImageRecognition.php
@@ -0,0 +1,31 @@
+<?php
+require 'vendor/autoload.php';
+use Aws\Sqs\SqsClient;
+use Aws\Credentials\CredentialProvider;
+use Aws\S3\S3Client;
+
+echo 'your input is ';
+echo $_GET['input'];
+
+$queue = SqsClient::factory(array(
+    'version' => 'latest',
+    'region'  => 'us-west-1',
+    'credentials' => array(
+    	'key' => 'AKIAJMVZ7WZA37QWGEBQ',
+    	'secret'  => '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',
+    )
+));
+
+$input_queue = 'https://sqs.us-west-1.amazonaws.com/858081815435/Input_Queue';
+
+$req = new stdClass();
+$req->id = rand();
+$req->input = $_GET['input'];
+$req_json = json_encode($req);
+
+$queue->sendMessage(array(
+    'QueueUrl'    => $input_queue,
+    'MessageBody' => $req_json,
+));
+
+?>
